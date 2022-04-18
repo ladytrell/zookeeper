e1aec57bd85a7644d96d94c7343fbb3d1fdfa7a4
Lady Trell <antrellkent@gmail.com>
2022-04-17 21:51:34 -0400

    accept user data

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+// https://lit-tor-15505.herokuapp.com/
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 // Use Heroku process.env.PORT if set, else use 3001
 const PORT = process.env.PORT || 3001;
 // Instantiate the server
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 // Deconstruct animals from the Json file
 const { animals } = require('./data/animals');
 
@@ -52,7 +60,34 @@ function findById(id, animalsArray) {
   return result;
 }
 
-// Route to all animal data
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// Route to collect all animal data
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -61,7 +96,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
-// Route to animal data by id
+// Route to collect animal data by id
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
@@ -69,6 +104,20 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
       res.send(404);
     }
+});
+
+// Route to accept data from a client
+app.post('/api/animals', (req, res) => {
+   // set id based on what the next index of the array will be
+   req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 // Listen on port 3001
