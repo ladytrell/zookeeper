41a24f96d3fe137ffb784389429dcaac17d85e36
Lady Trell <antrellkent@gmail.com>
2022-04-20 22:07:59 -0400

    Route testing

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const animalRoutes = require('../apiRoutes/animalRoutes');
 
 // Use the routes defined in apiRoutes/animalRoutes
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
